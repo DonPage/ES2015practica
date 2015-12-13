08a43a46e74f4aca03d6dad054138481f95088a9
DonPage <don.page.work@gmail.com>
2015-12-13 00:45:15 -0500

    destructure

--- test/spec/test.js
+++ test/spec/test.js
@@ -37,6 +37,59 @@
             expect(MAX_SIZE).toBe(10);
 
         })
-    })
+    });
+
+
+    /**
+     * @Destructuring
+     * Operation allows you to assign values to a set of variables.
+     */
+    describe('destructuring', function () {
+        'use strict';
+        it('can destructure arrays', function () {
+
+            let [, x, y, z] = [1, 3, 2, 10];
+            console.log([, x, y]);
+
+            expect(x).toBe(3);
+            expect(y).toBe(2);
+        });
+
+        it('can destructure objects', function () {
+
+            let doWork = function () {
+                return {
+                    firstName: 'don',
+                    lastName: 'page',
+                    twitter: 'Code'
+                };
+            };
+
+            let {
+                firstName: firstName,
+                twitter: twitter
+                } = doWork();
+
+            expect(firstName).toBe('don');
+
+        });
+
+        it('works with parameters', function () {
+
+            let doWork = function (url, {data, cache}) {
+                console.log(data);
+                return data
+
+            };
+
+            let results = doWork(
+                'api/test',
+                {data: 'test', cache: false}
+            );
+
+            expect(results).toBe('test');
+
+        });
+    });
 
 })();
