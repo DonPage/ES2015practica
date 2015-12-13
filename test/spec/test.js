2cfb3adf2e9f39f8061e47bab03da16a32e13683
DonPage <don.page.work@gmail.com>
2015-12-12 23:44:09 -0500

    let test passing and added fail comments.

--- test/spec/test.js
+++ test/spec/test.js
@@ -9,16 +9,18 @@
      * Let will provide block scoping
      */
     describe("how let works", function () {
-
-        var doWork = function (flag) {
-            if (flag) {
-                let x = 3;
-            }
-            return x;
-        };
-        let result = doWork(true);
-        expect(result).toBe(3);
-
+        it('will provide block scoping, unlike var', function () {
+            var doWork = function (flag) {
+                if (flag) {
+                    let x = 3;
+                    return x;
+                }
+                //line below will fail since x is scoped to the if statement.
+                //return x;
+            };
+            let result = doWork(true);
+            expect(result).toBe(3);
+        });
     });
 
 
