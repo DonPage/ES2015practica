666e47ca850b3b1f3c59754766f6f46a4081b913
DonPage <don.page.work@gmail.com>
2015-12-13 15:23:14 -0500

    getters and setters.

--- test/spec/classes.js
+++ test/spec/classes.js
@@ -26,7 +26,8 @@
             expect(e.getName()).toBe('Don');
 
         });
-        it('can create a class', function(){
+
+        it('can have constructor', function(){
 
             class Employee {
 
@@ -48,6 +49,36 @@
 
             expect(e1.getName()).toBe('Don');
             expect(e2.getName()).toBe('Alex');
+        });
+
+        it('can have getters and setters', function(){
+
+            class Employee {
+
+                constructor(name) {
+                    this._name = name;
+                }
+
+                doWork() {
+                    return 'complete!'
+                }
+
+                get name() {
+                    return this._name;
+                }
+
+                set name(newVal) {
+                    this._name = newVal;
+                }
+            }
+
+            let e1 = new Employee('Don');
+            let e2 = new Employee('Alex');
+
+            e2.name = 'Chris';
+
+            expect(e1.name).toBe('Don');
+            expect(e2.name).toBe('Chris');
         });
     });
 })();
