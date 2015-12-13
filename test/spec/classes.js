f024651e356815646001a20e3ddc8bd1724a39e1
DonPage <don.page.work@gmail.com>
2015-12-13 02:20:20 -0500

    constructor

--- test/spec/classes.js
+++ test/spec/classes.js
@@ -25,6 +25,29 @@
             expect(e.doWork()).toBe('complete!');
             expect(e.getName()).toBe('Don');
 
-        })
+        });
+        it('can create a class', function(){
+
+            class Employee {
+
+                constructor(name) {
+                    this._name = name;
+                }
+
+                doWork() {
+                    return 'complete!'
+                }
+
+                getName() {
+                    return this._name;
+                }
+            }
+
+            let e1 = new Employee('Don');
+            let e2 = new Employee('Alex');
+
+            expect(e1.getName()).toBe('Don');
+            expect(e2.getName()).toBe('Alex');
+        });
     });
 })();
